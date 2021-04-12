2503796fea4c0ba4074cc87f6528399d53f67524
costa <kossguzenko@gmail.com>
2021-04-12 19:21:16 +0300

    comit 22

---
+++ comp/newApp.js
@@ -0,0 +1,18 @@
+const fetchData = {
+  GeneralSchedule: {
+    Monday: ['9:00', '14:00', 'gMonday'],
+    Tuesday: ['10:00', '16:00', 'gTuesday'],
+    Wednesday: ['11:00', '16:00', 'gWednesday'],
+    Thursday: ['12:00', '16:00', 'gThursday'],
+    Friday: ['13:00', '18:00', 'gFriday'],
+    Saturday: ['14:00', '16:00', 'gSaturday'],
+  },
+  WorkshopSchedule: {
+    Monday: ['9:00', '14:00', 'wMonday'],
+    Tuesday: ['10:00', '16:00', 'wTuesday'],
+    Wednesday: ['11:00', '16:00', 'wWednesday'],
+    Thursday: ['12:00', '16:00', 'wThursday'],
+    Friday: ['13:00', '18:00', 'wFriday'],
+    Saturday: ['14:00', '16:00', 'wSaturday'],
+  },
+};
